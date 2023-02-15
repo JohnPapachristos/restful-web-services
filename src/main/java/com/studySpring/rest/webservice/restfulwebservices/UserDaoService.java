5a1fc07f80c80c709fafae88b6cbb26de96ee561
papachio <Ioannis.Papachristos@arhs-dev-hellas.com>
2023-02-15 17:42:03 +0200

    Added a new User by using post.

--- src/main/java/com/studySpring/rest/webservice/restfulwebservices/UserDaoService.java
+++ src/main/java/com/studySpring/rest/webservice/restfulwebservices/UserDaoService.java
@@ -11,15 +11,23 @@ public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<>();
 	
+	private static int counterId = 0;
+	
 	static {
-		users.add(new User(1,"John",LocalDate.now().minusYears(20)));
-		users.add(new User(2,"George",LocalDate.now().minusYears(30)));
-		users.add(new User(3,"Kostas",LocalDate.now().minusYears(15)));
-		users.add(new User(4,"Ria",LocalDate.now().minusYears(28)));
+		users.add(new User(++counterId,"John",LocalDate.now().minusYears(20)));
+		users.add(new User(++counterId,"George",LocalDate.now().minusYears(30)));
+		users.add(new User(++counterId,"Kostas",LocalDate.now().minusYears(15)));
+		users.add(new User(++counterId,"Ria",LocalDate.now().minusYears(28)));
 	}
 	
 	public List<User> findAll(){
 		return users;
+	}
+	
+	public User save(User user) {
+		user.setId(++counterId);
+		users.add(user);
+		return user;
 	}
 	
 }
