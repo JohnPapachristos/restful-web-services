587f9081b663f05d5407650dba6b0c671d5f8ea2
papachio <Ioannis.Papachristos@arhs-dev-hellas.com>
2023-02-15 16:50:39 +0200

    Best way to map the request.

--- src/main/java/com/studySpring/rest/webservice/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/studySpring/rest/webservice/restfulwebservices/HelloWorldController.java
@@ -1,14 +1,13 @@
 package com.studySpring.rest.webservice.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
 	
-	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
 	private String retrieveAnswer() {
 		return "Hello-world!";
 	}
