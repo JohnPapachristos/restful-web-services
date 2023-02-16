abbb5b41ac5537f554a9d58319afdf73477fe039
papachio <Ioannis.Papachristos@arhs-dev-hellas.com>
2023-02-16 16:33:41 +0200

    Validation are added to the code when we try to insert a new user.

--- src/main/java/com/studySpring/rest/webservice/restfulwebservices/UserController.java
+++ src/main/java/com/studySpring/rest/webservice/restfulwebservices/UserController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class UserController {
 
@@ -37,7 +39,7 @@ public class UserController {
 	}
 	
 	@PostMapping(path = "/users")
-	public ResponseEntity<User> createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
 		User savedUser = userDaoService.save(user);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 		return ResponseEntity.created(location).build();
